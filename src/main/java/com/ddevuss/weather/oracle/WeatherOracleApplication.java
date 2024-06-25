418b4e8d7f4c46f904f9a41f31e5148fc019bb0c
dDevusS <geckahilden@gmail.com>
2024-06-24 22:04:46 -0400

    build: Create new project weather-oracle with Spring boot

---
+++ src/main/java/com/ddevuss/weather/oracle/WeatherOracleApplication.java
@@ -0,0 +1,13 @@
+package com.ddevuss.weather.oracle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherOracleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherOracleApplication.class, args);
+    }
+
+}
