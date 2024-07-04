3bc6239b4ba2c412e2391c45ca9b15dac20a040f
dDevusS <geckahilden@gmail.com>
2024-07-04 18:35:01 -0400

    test/refactor: Move @SpringBootTest to @IntegrationTest.

--- src/test/java/com/ddevuss/weather/oracle/annotation/IntegrationTest.java
+++ src/test/java/com/ddevuss/weather/oracle/annotation/IntegrationTest.java
@@ -1,5 +1,6 @@
 package com.ddevuss.weather.oracle.annotation;
 
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,4 +13,5 @@ import java.lang.annotation.Target;
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @Transactional
+@SpringBootTest
 public @interface IntegrationTest {}
