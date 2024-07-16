d7bf58bfecc29578e1e96ef73dc4d5491d07ff14
dDevusS <evgenii@kashin@gmail.com>
2024-07-15 20:01:30 -0400

    fix(LocationReadDto): implement Serializable to LocationReadDto

--- src/main/java/com/ddevuss/weather/oracle/dto/LocationReadDto.java
+++ src/main/java/com/ddevuss/weather/oracle/dto/LocationReadDto.java
@@ -3,11 +3,12 @@ package com.ddevuss.weather.oracle.dto;
 import lombok.Builder;
 import lombok.Value;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
 
 @Value
 @Builder
-public class LocationReadDto {
+public class LocationReadDto implements Serializable {
 
     Integer id;
 
