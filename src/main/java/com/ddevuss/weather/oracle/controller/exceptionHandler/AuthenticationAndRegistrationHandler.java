2427c9108feaed069465f94033a51c35792034a2
dDevusS <evgenii@kashin@gmail.com>
2024-07-17 13:14:39 -0400

    feat(AuthenticationAndRegistrationHandler): add handling new scripts
    * handle BadCredentialExeption
    * handle AuthenticationServiceException

--- src/main/java/com/ddevuss/weather/oracle/controller/exceptionHandler/AuthenticationAndRegistrationHandler.java
+++ src/main/java/com/ddevuss/weather/oracle/controller/exceptionHandler/AuthenticationAndRegistrationHandler.java
@@ -1,6 +1,8 @@
 package com.ddevuss.weather.oracle.controller.exceptionHandler;
 
 import com.ddevuss.weather.oracle.exception.LoginNotUniqueException;
+import org.springframework.security.authentication.AuthenticationServiceException;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -9,9 +11,26 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 public class AuthenticationAndRegistrationHandler {
 
     @ExceptionHandler(value = {LoginNotUniqueException.class})
-    public String handleUsernameNotFoundException(LoginNotUniqueException e,
-                                                  RedirectAttributes redirectAttributes) {
+    public String handleLoginNotUniqueException(LoginNotUniqueException e,
+                                                RedirectAttributes redirectAttributes) {
         redirectAttributes.addFlashAttribute("errorMessage", e.getMessage());
         return "redirect:/registration";
     }
+
+    @ExceptionHandler(value = {BadCredentialsException.class})
+    public String handleBadCredentialsException(BadCredentialsException e,
+                                                RedirectAttributes redirectAttributes) {
+        redirectAttributes.addFlashAttribute("errorMessage",
+                "Invalid login or password.");
+        return "redirect:/login";
+    }
+
+    @ExceptionHandler(value = {AuthenticationServiceException.class})
+    public String handleAuthenticationServiceException(AuthenticationServiceException e,
+                                                       RedirectAttributes redirectAttributes) {
+        redirectAttributes.addFlashAttribute("errorMessage",
+                "Something went wrong during authentication due to an error in the DB. Please try again later.");
+        return "redirect:/login";
+    }
+
 }
