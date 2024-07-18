0545062e0d8724b599541d3a83972d06a765ea69
dDevusS <evgenii@kashin@gmail.com>
2024-07-18 16:41:33 -0400

    feat(MainController): add resolving DuplicatedLocationException to 'location/save'

--- src/main/java/com/ddevuss/weather/oracle/controller/MainController.java
+++ src/main/java/com/ddevuss/weather/oracle/controller/MainController.java
@@ -5,15 +5,20 @@ import com.ddevuss.weather.oracle.dto.UserInfoDto;
 import com.ddevuss.weather.oracle.dto.UserSessionInfoDto;
 import com.ddevuss.weather.oracle.entity.Location;
 import com.ddevuss.weather.oracle.entity.User;
+import com.ddevuss.weather.oracle.exception.DuplicatedLocationException;
 import com.ddevuss.weather.oracle.service.LocationService;
 import com.ddevuss.weather.oracle.service.MainService;
 import com.ddevuss.weather.oracle.service.OpenWeatherService;
 import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
 @AllArgsConstructor
 @SessionAttributes({
@@ -47,31 +52,44 @@ public class MainController {
 
     @GetMapping("location/search")
     public String searchLocation(Model model,
-                                 @RequestParam(required = false) String locationName) {
+                                 @RequestParam(required = false) String locationName,
+                                 @RequestParam(required = false) String errorMessage) {
         if (locationName == null || locationName.isBlank()) {
             model.addAttribute("blankParameterWarning",
                     "Name of location was empty.");
             return "searching";
         }
 
+        model.addAttribute("errorMessage", errorMessage);
+        model.addAttribute("locationName", locationName);
         locationName = locationName.trim();
-        LocationResponseDto[] locations = openWeatherService.searchLocationByName(locationName);
+        LocationResponseDto[] locations = openWeatherService.searchLocationsByName(locationName);
         model.addAttribute("locations", locations);
         return "searching";
     }
 
+    @SneakyThrows
     @PostMapping("location/save")
-    public String saveLocation(@ModelAttribute("location") LocationResponseDto locationResponseDto,
+    public String saveLocation(Model model,
+                               @ModelAttribute("locationName") String locationName,
+                               @ModelAttribute("location") LocationResponseDto locationResponseDto,
                                @SessionAttribute("userInfo") UserSessionInfoDto userInfo) {
-        locationService.save(Location.builder()
-                .user(User.builder()
-                        .id(userInfo.getId())
-                        .login(userInfo.getLogin())
-                        .build())
-                .name(locationResponseDto.getName())
-                .latitude(locationResponseDto.getLat())
-                .longitude(locationResponseDto.getLon())
-                .build());
+        try {
+            locationService.save(Location.builder()
+                    .user(User.builder()
+                            .id(userInfo.getId())
+                            .login(userInfo.getLogin())
+                            .build())
+                    .name(locationResponseDto.getName())
+                    .latitude(locationResponseDto.getLat())
+                    .longitude(locationResponseDto.getLon())
+                    .build());
+        }
+        catch (DuplicatedLocationException e) {
+            String encodedLocationName = URLEncoder.encode(locationName, StandardCharsets.UTF_8);
+            String encodedErrorMessage = URLEncoder.encode("This location already exists", StandardCharsets.UTF_8);
+            return "redirect:/location/search?locationName=" + encodedLocationName + "&errorMessage=" + encodedErrorMessage;
+        }
 
         return "redirect:/";
     }
