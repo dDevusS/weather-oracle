7ada34d83842e346562de82229052956afd77bb3
dDevusS <evgenii@kashin@gmail.com>
2024-07-18 16:36:22 -0400

    feat(LocationRepository): add method findByUserIdAndLocationLatitudeAndLocationLongitude

--- src/main/java/com/ddevuss/weather/oracle/repository/LocationRepository.java
+++ src/main/java/com/ddevuss/weather/oracle/repository/LocationRepository.java
@@ -3,8 +3,10 @@ package com.ddevuss.weather.oracle.repository;
 import com.ddevuss.weather.oracle.entity.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface LocationRepository extends JpaRepository<Location, Long> {
 
@@ -12,5 +14,11 @@ public interface LocationRepository extends JpaRepository<Location, Long> {
 
     @Modifying
     int deleteAllByUserId(Long userId);
+
+    @Query(value = "select l from Location l " +
+                   "where l.user.id = :userId " +
+                   "and l.latitude = :latitude " +
+                   "and  l.longitude = :longitude")
+    Optional<Location> findByUserIdAndLocationLatitudeAndLocationLongitude(Long userId, Double latitude, Double longitude);
 
 }
