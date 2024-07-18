6837bd222d6f7bedc40d75550f0beb4df94d170a
dDevusS <evgenii@kashin@gmail.com>
2024-07-18 00:14:46 -0400

    chore(AuthenticationAndRegistrationHandler): implement logging

--- src/main/java/com/ddevuss/weather/oracle/controller/exceptionHandler/AuthenticationAndRegistrationHandler.java
+++ src/main/java/com/ddevuss/weather/oracle/controller/exceptionHandler/AuthenticationAndRegistrationHandler.java
@@ -1,12 +1,14 @@
 package com.ddevuss.weather.oracle.controller.exceptionHandler;
 
 import com.ddevuss.weather.oracle.exception.LoginNotUniqueException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationServiceException;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+@Slf4j
 @ControllerAdvice
 public class AuthenticationAndRegistrationHandler {
 
@@ -29,6 +31,7 @@ public class AuthenticationAndRegistrationHandler {
     @ExceptionHandler(value = {AuthenticationServiceException.class})
     public String handleAuthenticationServiceException(AuthenticationServiceException e,
                                                        RedirectAttributes redirectAttributes) {
+        log.error("AuthenticationServiceException", e);
         redirectAttributes.addFlashAttribute("errorMessage",
                 "Something went wrong during authentication due to an error in the DB. Please try again later.");
         return "redirect:/login";
