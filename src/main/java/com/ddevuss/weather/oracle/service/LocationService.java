0323de0c17080946fc3e7c289f97497b861e75ec
dDevusS <evgenii@kashin@gmail.com>
2024-07-21 05:27:12 -0400

    perf(LocationService): add checking on existing user before save method and change method findAllByUserId to findAllByUserLogin
    * remove secure checking before save method due to it's not necessary in new context

--- src/main/java/com/ddevuss/weather/oracle/service/LocationService.java
+++ src/main/java/com/ddevuss/weather/oracle/service/LocationService.java
@@ -2,9 +2,11 @@ package com.ddevuss.weather.oracle.service;
 
 import com.ddevuss.weather.oracle.dto.LocationReadDto;
 import com.ddevuss.weather.oracle.entity.Location;
+import com.ddevuss.weather.oracle.entity.User;
 import com.ddevuss.weather.oracle.exception.NotUniqueLocationException;
 import com.ddevuss.weather.oracle.mapper.LocationReadDtoFromEntityMapper;
 import com.ddevuss.weather.oracle.repository.LocationRepository;
+import com.ddevuss.weather.oracle.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
@@ -17,18 +19,20 @@ import java.util.List;
 @Service
 public class LocationService {
 
+    private final UserRepository userRepository;
     private final LocationRepository locationRepository;
     private final LocationReadDtoFromEntityMapper locationReadDtoFromEntityMapper;
 
-    public List<LocationReadDto> findAllByUserId(Long userId) {
-        return locationRepository.findAllByUserId(userId).stream()
+    public List<LocationReadDto> findAllByUserLogin(String login) {
+        return locationRepository.findAllByUserLogin(login).stream()
                 .map(locationReadDtoFromEntityMapper::entityToDto)
                 .toList();
     }
 
     @Transactional
-    @PreAuthorize("@securityService.hasPermissionToSaveLocation(#location)")
     public void save(Location location) {
+        User user = userRepository.findByLogin(location.getUser().getLogin()).orElseThrow();
+        location.setUser(user);
         ensureUniqueLocationForUser(location);
         locationReadDtoFromEntityMapper.entityToDto(locationRepository.save(location));
     }
