f5a6fa1b24391fe785e7b8afa735d1a33d2d3bae
dDevusS <evgenii@kashin@gmail.com>
2024-07-21 05:37:21 -0400

    feat(SecurityConfiguration): add permition for guests on '/css/**', '/js/**', '/img/**', 'fragments'

--- src/main/java/com/ddevuss/weather/oracle/configuration/SecurityConfiguration.java
+++ src/main/java/com/ddevuss/weather/oracle/configuration/SecurityConfiguration.java
@@ -18,7 +18,14 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(request -> request
-                        .requestMatchers("/login", "/registration", "/", "error").permitAll()
+                        .requestMatchers("/login",
+                                "/registration",
+                                "/",
+                                "error",
+                                "/css/**",
+                                "/js/**",
+                                "/img/**",
+                                "fragments").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin(logging -> logging
