bf765d13a717bbf642b0aad026ec19832250f302
dDevusS <evgenii@kashin@gmail.com>
2024-07-26 21:58:52 -0400

    feat(SecurityConfiguration): enable csrf filter

--- src/main/java/com/ddevuss/weather/oracle/configuration/SecurityConfiguration.java
+++ src/main/java/com/ddevuss/weather/oracle/configuration/SecurityConfiguration.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -44,8 +43,7 @@ public class SecurityConfiguration {
                         .invalidSessionUrl("/forecast")
                         .maximumSessions(3)
                         .expiredUrl("/forecast")
-                )
-                .csrf(AbstractHttpConfigurer::disable);
+                );
 
         return http.build();
     }
