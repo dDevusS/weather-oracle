e94820797aa0fd95618b25579cb8cb2601ecef4c
ddevuss <evgenii69kashin@gmail.com>
2025-07-17 23:32:04 -0400

    feat: change PostMapping '/location/delete' to DeleteMapping '/location/{id}'

--- src/main/java/com/ddevuss/weather/oracle/controller/api/LocationRestController.java
+++ src/main/java/com/ddevuss/weather/oracle/controller/api/LocationRestController.java
@@ -17,7 +17,9 @@ import org.springframework.data.domain.Slice;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -87,8 +89,8 @@ public class LocationRestController implements LocationController {
         }
     }
 
-    @PostMapping("/location/delete")
-    public ResponseEntity<?> deleteLocation(@RequestBody Long locationId) {
+    @DeleteMapping("/location/{id}")
+    public ResponseEntity<?> deleteLocation(@PathVariable("id") Long locationId) {
         try {
             locationService.deleteById(locationId);
             return ResponseEntity.noContent().build();
