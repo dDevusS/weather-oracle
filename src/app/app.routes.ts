3e95886436a08310e0feb94d84566330a1dd9a4e
ddevuss <evgenii69kashin@gmail.com>
2025-08-03 20:32:38 -0400

    feat: setup routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,33 @@
 import { Routes } from '@angular/router';
 import {Layout} from './shared/layout/layout';
+import {LoginPage} from './pages/auth/login-page/login-page';
+import {RegistrationPage} from './pages/auth/registration-page/registration-page';
+import {SearchPage} from './pages/search-page/search-page';
+import {accessGuard} from './pages/auth/access.guard';
+import {ForecastPage} from './pages/forecast-page/forecast-page';
 
 export const routes: Routes = [
   {
     path: '',
     component: Layout,
     children: [
-
+      {
+        path: '',
+        component: ForecastPage
+      },
+      {
+        path: 'login',
+        component: LoginPage
+      },
+      {
+        path: 'registration',
+        component: RegistrationPage
+      },
+      {
+        path: 'search',
+        component: SearchPage,
+        canActivate: [accessGuard]
+      }
     ]
   }
 ];
